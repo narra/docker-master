68ce4478a96d3d25590ca83d05e977c7e1a032da
Michal Mocnak <michal@marigan.net>
2020-04-26 16:19:37 +0200

    typo in scripts, narra changed to platform

--- scripts/narra_debug.sh
+++ scripts/narra_debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # NARRA Debug
 
-cd /home/app/source/narra
+cd /home/app/source/platform
 
 if [ $DEBUG == "true" ]; then
-  exec /sbin/setuser app /usr/bin/bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /home/app/source/narra/bin/rails s -b 0.0.0.0 -p 8080 -e development
+  exec /sbin/setuser app /usr/bin/bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /home/app/source/platform/bin/rails s -b 0.0.0.0 -p 8080 -e development
 fi
