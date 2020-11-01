11cbf8e9407669424c7fcd4d9fe71a21203be4d1
Michal Mocnak <michal@marigan.net>
2020-11-01 11:31:48 +0100

    adding support for SCOUT_APM, setting server port to 80

--- scripts/narra_debug.sh
+++ scripts/narra_debug.sh
@@ -4,5 +4,5 @@
 cd /home/app/source/platform
 
 if [ $DEBUG == "true" ]; then
-  exec /sbin/setuser app /usr/bin/bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /home/app/source/platform/bin/rails s -b 0.0.0.0 -p 8080 -e development
+  exec /sbin/setuser app /usr/bin/bundle exec rdebug-ide --host 0.0.0.0 --port 1234 --dispatcher-port 26162 -- /home/app/source/platform/bin/rails s -b 0.0.0.0 -p 80 -e development
 fi
